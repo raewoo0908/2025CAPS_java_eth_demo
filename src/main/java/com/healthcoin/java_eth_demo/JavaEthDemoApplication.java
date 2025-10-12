333b09bd5ac665cd82fee83ff0b97948c6e10ec9
Raewoo Kang <raewookang@Raewooui-MacBookAir.local>
2025-10-12 13:39:40 +0900

    first commit

---
+++ src/main/java/com/healthcoin/java_eth_demo/JavaEthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.healthcoin.java_eth_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEthDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healthcoin/java_eth_demo/JavaEthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcoin.java_eth_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEthDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
